8900b69da0419039f94aa6b5ddc76425f1db2577
Michael Arntzenius <daekharel@gmail.com>
2021-08-01 16:13:28 +0100

    don't use pop to quick macro in sleep mode

--- pop_to_quick_macro.py
+++ pop_to_quick_macro.py
@@ -1,7 +1,9 @@
 # This causes popping to run the quick macro. Rename this file to something like
 # pop_to_quick_macro.py.ignore to avoid this behavior, or just comment out the
 # whole file.
-from talon import noise, actions
+from talon import noise, actions, scope
 def on_pop(active):
-    actions.user.quick_macro_run()
+    modes = scope.get("mode")
+    if "sleep" not in modes:
+        actions.user.quick_macro_run()
 noise.register("pop", on_pop)
